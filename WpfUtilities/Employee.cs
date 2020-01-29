146b4c6a25aa9e27e79622439ab0a9746cb4400c
alan <alandias01@yahoo.com>
2020-01-28 22:43:42 -0500

    employee

--- WpfUtilities/Employee.cs
+++ WpfUtilities/Employee.cs
@@ -1,10 +1,52 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace WpfUtilities
 {
-    class Employee
+    public class Employee : INotifyPropertyChanged
     {
+        private string name;
+        int age;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+                
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                this.OnPropertyChanged();
+            }
+        }
+        
+        public int Age
+        {
+            get { return age; }
+            set 
+            { 
+                age = value;
+                this.OnPropertyChanged();
+            }
+        }
+
+        public Employee(string n, int a)
+        {
+            Name = n;
+            Age = a;
+        }
+
+        public static ObservableCollection<Employee> Load()
+        {
+            return new ObservableCollection<Employee> { new Employee("alan", 1), new Employee("Ben", 2), new Employee("Kyle", 3), new Employee("Stan", 4) };
+        }
+
+        // Create the OnPropertyChanged method to raise the event
+        // The calling member's name will be used as the parameter.
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
     }
 }
