ad9358d24dc57b8f26cb9590f7969288675ba6da
alan <alandias01@yahoo.com>
2023-10-20 01:01:53 -0400

    BulkObservableCollection

---
+++ WpfUtilities/Collections/BulkObservableCollection.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Text;
+
+namespace WpfUtilities.Collections
+{
+    public class BulkObservableCollection<T> : ObservableCollection<T>
+    {
+        private bool shouldRaiseCollectionChanged = true;
+        protected override void OnCollectionChanged(NotifyCollectionChangedEventArgs e)
+        {
+            if (this.shouldRaiseCollectionChanged)
+                base.OnCollectionChanged(e);
+        }
+
+        public void AddRange(IEnumerable<T> list)
+        {
+            this.shouldRaiseCollectionChanged = false;
+            foreach (T item in list)
+            {
+                this.Add(item);
+            }
+            this.shouldRaiseCollectionChanged = true;
+            this.OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
+        }
+    }
+}
